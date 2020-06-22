f889565d1f3f39120ac7763644093dde5cd7e98a
Wellington <wellingtonslpbi@hotmail.com>
2020-06-22 19:54:29 -0300

    add eslint, prettier and editor config to keep code compatibility between developers

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ hello: "World" });
+app.get('/', (req, res) => {
+  res.json({ hello: 'World' });
 });
 
 app.listen(3333, () => {
-  console.log("🚀 Server running on port 3333!");
+  console.log('🚀 Server running on port 3333!');
 });
