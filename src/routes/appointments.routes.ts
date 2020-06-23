2ad0952ed72ee64050aa2707d101cca41116a650
Wellington <wellingtonslpbi@hotmail.com>
2020-06-23 20:18:24 -0300

    aply DTO concept in appointment repository and apply Omit func in appointment model

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,4 +1,4 @@
-import { Router, response } from 'express';
+import { Router } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 
@@ -25,7 +25,10 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = appointmentRepository.create(provider, parsedDate);
+  const appointment = appointmentRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
