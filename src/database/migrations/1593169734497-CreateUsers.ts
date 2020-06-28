3fc612dbe8a281b6a5d9bef2976303ef0c451ed9
Wellington <wellingtonslpbi@hotmail.com>
2020-06-27 21:46:53 -0300

    alter provider field in appointments model for provider_id and create relationship for this

--- src/database/migrations/1593169734497-CreateUsers.ts
+++ src/database/migrations/1593169734497-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1593169734497 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
