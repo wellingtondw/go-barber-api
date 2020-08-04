718ac016cd1556e30c0c742fd4ae49400a455692
wellington <wellingtonslpbi@hotmail.com>
2020-08-04 12:01:10 -0300

    applying tdd

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmailService', () => {
+  it('should be able to recovery password using email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmailService = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmailService.execute({
+      email: 'johndoe@example.com',
+    });
+
+    expect(sendMail).toBeCalled();
+  });
+});
