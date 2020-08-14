3711124a029d4ab0559ffa3070b86308a4a9c452
wellington <wellingtonslpbi@hotmail.com>
2020-08-13 22:09:57 -0300

    creating appointment and relationship with user_id

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -16,11 +16,13 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '12345623423423',
       provider_id: '12345623423423',
     });
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('12345623423423');
+    expect(appointment.user_id).toBe('12345623423423');
   });
 
   it('should not be able to create two appointments on the same time', async () => {
@@ -28,12 +30,14 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '12345623423423',
       provider_id: '12345623423423',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '12345623423423',
         provider_id: '12345623423423',
       }),
     ).rejects.toBeInstanceOf(AppError);
